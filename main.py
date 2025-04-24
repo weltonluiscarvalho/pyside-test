8ee8bd1e49aa65301a2f374fd63dd2c9095c4d47
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-04-24 12:40:45 -0300

    testing some stylesheet concepts

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import sys
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication, QComboBox, QLabel, QMainWindow, QVBoxLayout, QWidget
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         central_widget = QWidget()
         layout = QVBoxLayout()
-        label = QLabel("Label 1")
+        label = QLabel("Mais vale um passaro na mao do que 2 voando")
         label2 = QLabel("Label 2")
         label3 = QLabel("Label 3")
 
-        label.setStyleSheet("border: 1px solid black")
-        label2.setStyleSheet("border: 1px solid black")
-        label3.setStyleSheet("border: 1px solid black")
+        style_sheet = """
+            QLabel {
+                border: 2px solid black;
+                background-image: url(gato.png);
+                background-position: right;
+                background-clip: content;
+                background-size: contain;
+            }
+        """
+        central_widget.setStyleSheet(style_sheet)
+        label.setStyleSheet("QLabel { border: 1px solid black }")
+
         
-        size = QSize(15,15)
-        label.setMaximumHeight(20)        
+        
+        # label2.setStyleSheet(style_sheet)
+
+        # label.setStyleSheet("border: 1px solid black; font-family: JetBrainsMonoNerdFont; font-size: 5.5em")
+        # label2.setStyleSheet("border: 1px solid black")
+        # label3.setStyleSheet("border: 1px solid black")
+        
         label2.setMaximumHeight(20)        
         label3.setMaximumHeight(20)        
         layout.addWidget(label)
