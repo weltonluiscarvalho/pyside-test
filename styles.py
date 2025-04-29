23d7ccabc5cff94e1281f30571b49a4cf1c79bcb
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-04-29 17:26:40 -0300

    drawing polygons

--- styles.py
+++ styles.py
@@ -1,5 +1,6 @@
-from PySide6.QtGui import QPainter, QPalette
-from PySide6.QtWidgets import QStyle, QStyleOptionFocusRect, QWidget
+from PySide6.QtCore import QPoint
+from PySide6.QtGui import QPainter, QPalette, QPolygon
+from PySide6.QtWidgets import QProxyStyle, QStyle, QStyleOptionFocusRect, QWidget
 
 class myWidget(QWidget):
     def __init__(self, parent):
@@ -14,4 +15,36 @@ class myWidget(QWidget):
         style().drawPrimitive(QStyle.PE_FrameFocusRect, option, painter, self)
 
 
-print('bunda')
+class CustomStyle(QProxyStyle):
+
+    def drawPrimitive(element, option, painter, widget):
+        if element == PE_IndicatorSpinUp or element == PE_IndicatorSpinDown:
+            points = QPolygon(3)
+            x = option.rect.x()
+            y = option.rect.y()
+            w = option.rect.width() / 2
+            h = option.rect.height() / 2
+            x += (option.rect.width() - w) / 2
+            y += (option.rect.height() - h) / 2
+            if element == PE_IndicatorSpinUp:
+                points[0] = QPoint(x, y + h)
+                points[1] = QPoint(x + w, y + h)
+                points[2] = QPoint(x + w / 2, y + h)
+
+            else:
+                points[0] = QPoint(x, y)
+                points[1] = QPoint(x + w, y)
+                points[2] = QPoint(x + w / 2, y + h)
+
+
+            if option.state == State_Enabled:
+                painter.setPen(option.palette.mid().color())
+                painter.setBrush(option.palette.buttonText())
+            else:
+                painter.setPen(option.palette.buttonText().color())
+                painter.setBrush(option.palette.mid())
+
+            painter.drawPolygon(points)
+
+        else:
+            QProxyStyle.drawPrimitive(element, option, painter, widget)
